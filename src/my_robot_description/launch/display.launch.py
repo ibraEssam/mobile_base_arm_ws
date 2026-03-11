be030b562018e9b5c5d36cf83eca1feff0f09871
Ibrahim <ibrahim.essam1995@gmail.com>
2026-03-11 02:45:21 +0000

    add robot variants to the display launch file

--- src/my_robot_description/launch/display.launch.py
+++ src/my_robot_description/launch/display.launch.py
@@ -1,12 +1,29 @@
 from launch_ros.actions import Node
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
-from launch.substitutions import Command
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch.actions import DeclareLaunchArgument
+
 def generate_launch_description():
-    urdf_file = get_package_share_directory('my_robot_description') + '/urdf/robot.urdf.xacro'
-    # Process the xacro file to generate the URDF
+    # choose from one of the available URDF/Xacro files in src/my_robot_description/urdf
+    robot_variant_arg = DeclareLaunchArgument(
+        'robot_variant',
+        default_value='platform',
+        choices=['arm_with_gripper', 'platform'],
+        description='Select URDF/xacro file to use for the robot. Options: "arm_with_gripper" or "platform". Default is "platform".'
+    )
+
+    urdf_file_name = LaunchConfiguration('robot_variant')
+    urdf_file = PathJoinSubstitution([
+        get_package_share_directory('my_robot_description'),
+        'urdf',
+        [urdf_file_name,
+        ".urdf.xacro"]
+    ])
+
+    # Process the chosen xacro file to generate the final URDF
     urdf_content = Command(['xacro ', urdf_file])
-    
+
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -15,7 +32,6 @@ def generate_launch_description():
         parameters=[{'robot_description': urdf_content}]
     )
 
-    # joint state publisher gui node
     joint_state_publisher_node = Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
@@ -23,7 +39,6 @@ def generate_launch_description():
         output='screen'
     )
 
-    # rviz node
     rviz_config_file = get_package_share_directory('my_robot_description') + '/rviz/robot_display.rviz'
     rviz_node = Node(
         package='rviz2',
@@ -34,6 +49,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        robot_variant_arg,
         robot_state_publisher_node,
         joint_state_publisher_node,
         rviz_node
