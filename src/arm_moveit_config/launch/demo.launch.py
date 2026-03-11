d9d71d753dcfac98c0b42dc58efb14d1e6c5ee6d
Ibrahim <ibrahim.essam1995@gmail.com>
2026-03-11 02:44:54 +0000

    rename the arm package to arm_movit

--- src/arm_moveit_config/launch/demo.launch.py
+++ src/arm_moveit_config/launch/demo.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("my_arm", package_name="my_robot_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("my_arm", package_name="arm_moveit_config").to_moveit_configs()
     return generate_demo_launch(moveit_config)
--- src/arm_moveit_config/launch/moveit_rviz.launch.py
+++ src/arm_moveit_config/launch/moveit_rviz.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_moveit_rviz_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("my_arm", package_name="my_robot_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("my_arm", package_name="arm_moveit_config").to_moveit_configs()
     return generate_moveit_rviz_launch(moveit_config)
